ac2d35d68a8330065c1657fa26025f0dc182c410
ponckamil <ponc.kamil@gmail.com>
2023-10-01 08:05:41 +0200

    Added Icons and Notification Screen

--- App/GWIZD-AR/Assets/Source/UI/MainController.cs
+++ App/GWIZD-AR/Assets/Source/UI/MainController.cs
@@ -1,4 +1,5 @@
 using Mono.Cecil.Cil;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,6 +15,7 @@ public class MainController : MonoBehaviour
     private VisualElement[] buttons;
     public VisualTreeAsset alertDocument;
     public VisualTreeAsset profileDocument;
+    public VisualTreeAsset notificationDocument;
 
 
     // Start is called before the first frame update
@@ -21,14 +23,24 @@ public class MainController : MonoBehaviour
     {
         document = GetComponent<UIDocument>();
 
-        buttons = new VisualElement[3];
+        buttons = new VisualElement[4];
         buttons[0] = document.rootVisualElement.Q<VisualElement>("MapBtn");
         buttons[1] = document.rootVisualElement.Q<VisualElement>("AlertBtn");
         buttons[2] = document.rootVisualElement.Q<VisualElement>("ProfileBtn");
+        buttons[3] = document.rootVisualElement.Q<VisualElement>("NotificationBtn");
+
 
         buttons[0].RegisterCallback<ClickEvent>(MapBtnOnClick);
         buttons[1].RegisterCallback<ClickEvent>(AlertBtnOnClick);
         buttons[2].RegisterCallback<ClickEvent>(ProfileBtnOnClick);
+        buttons[3].RegisterCallback<ClickEvent>(NotificationBtnOnClick);
+
+    }
+
+    private void NotificationBtnOnClick(ClickEvent evt)
+    {
+        centerUIController.SetDocument(notificationDocument);
+
     }
 
     private void ProfileBtnOnClick(ClickEvent evt)
